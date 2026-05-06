89092c5fc56c112af5e0d92f324a32e6a3862790
faisalkbr66 <fa887414@gmail.com>
2026-05-06 22:29:03 +0700

    add eslint and prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import {ThemeProvider, CssBaseline, createTheme, Button} from '@mui/material'
+import { Button, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 
 const theme = createTheme({
   typography: {
-    fontFamily: ["Roboto","sans-serif"].join(",")
-  }
-})
+    fontFamily: ["Roboto", "sans-serif"].join(","),
+  },
+});
 
 const Home = () => {
-  return(
-    <Button type='button' variant='contained'> Halo, Ini tombol MUI!</Button>
-  )
-}
+  return (
+    <Button type="button" variant="contained">
+      Halo, Ini tombol MUI!
+    </Button>
+  );
+};
 
 const App = () => {
-  return(
+  return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Home />
     </ThemeProvider>
-  )
-}
+  );
+};
 
 export default App;
